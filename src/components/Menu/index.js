9863cbab3b4ede777bee569e49693be88e2e427d
Paulo Carmino <contato@paulocarmino.com>
2019-05-07 23:26:17 -0300

    Adds Menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import QRCode from 'react-native-qrcode';
+
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles';
+
+export default function Menu() {
+  return (
+    <Container>
+      <Code>
+        <QRCode
+          value="http://github.com/paulocarmino"
+          size={80}
+          bgColor="#8B10AE"
+          fgColor="#FFF"
+        />
+      </Code>
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me Ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem><NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar Cartão</NavText>
+        </NavItem><NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configurações do App</NavText>
+        </NavItem>
+      </Nav>
+
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>
+          SAIR DO APP
+        </SignOutButtonText>
+      </SignOutButton>
+    </Container>
+  );
+}
