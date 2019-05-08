35f2ebd5b68c74bc97aafb64ad840cf712d6b289
Paulo Carmino <contato@paulocarmino.com>
2019-05-07 23:12:49 -0300

    Finish Card Home

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header'
 import Tabs from '~/components/Tabs'
+import Menu from '~/components/Menu'
 
-import { Container } from './styles';
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 export default function Main() {
   return (
@@ -11,10 +13,22 @@ export default function Main() {
       <Header />
 
       <Content>
+        <Menu/>
+
         <Card>
-          <CardHeader></CardHeader>
-          <CardContent></CardContent>
-          <CardFooter></CardFooter>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666"/>
+            <Icon name="visibility-off" size={28} color="#666"/>
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo Disponível</Title>
+            <Description>R$ 8.234,39</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de 6232,50 reais recebida de Futuro Trampo hoje às 06:23h
+            </Annotation>
+          </CardFooter>
         </Card>
       </Content>
 
